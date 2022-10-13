d080103b24c09ccce067aa15172ba1883db78e67
leopvr <leo.pvr13@gmail.com>
2022-10-13 18:13:11 -0300

    :tada: Feat: new repo for JS exercises

---
+++ script.js
@@ -0,0 +1,28 @@
+var nombre = "Leo"
+var edad = 21
+var enPareja = true
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var colegas = ["Fabi", "Silvi", "Carla", "Sabri", "Patri"]
+
+var object = {
+    nombre: "Leo",
+    edad: 21,
+    enPareja: true,
+    numeros: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    colegas: ["Fabi", "Silvi", "Carla", "Sabri", "Patri"]
+}
+
+var infoUsuario = {
+    numFavs: [6, 5, 4],
+    perro: {
+        nombre: "Cani",
+        edad: 5
+    },
+    gato: {
+        nombre: "Cati",
+        edad: 3
+    }
+}
+
+console.log(object)
+console.log(infoUsuario)
